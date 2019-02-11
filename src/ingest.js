80aad765e4837d9f45757fe7a8c4ae78de51176e
edgrams <edgramswork@gmail.com>
2019-02-11 14:49:48 -0600

    nvmrc + init

--- src/ingest.js
+++ src/ingest.js
@@ -1,8 +1,22 @@
-const { Pool } = require('pg');
+const createTable = "CREATE TABLE users (ID SERIAL PRIMARY KEY, name VARCHAR(30));";
+const tableExistsQuery = "SELECT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'users')";
+const { Client } = require('pg');
 
-const pool = new Pool();
+const initialize = async () => {
+    const client = new Client();
+    await client.connect();
+    const res = await client.query(tableExistsQuery);
+    console.log("response: " + JSON.stringify(res.rows[0]));
+    if (!res.rows[0].exists) {
+        const res = await client.query(createTable);
+        console.log("response: " + JSON.stringify(res.rows[0]));
+    }
+    await client.end();
+};
 
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res);
-    pool.end()
-});
+initialize().then(() => {
+    console.log("Load completed!");
+    process.exit();
+}).catch(
+    e => console.error(e.stack)
+);
