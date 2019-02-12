ac14f5814a927ea2e46d150d6d0a2845ebb8b193
edgrams <edgramswork@gmail.com>
2019-02-11 20:08:08 -0600

    clean up

--- src/schedule-loader.js
+++ src/schedule-loader.js
@@ -2,22 +2,27 @@ const { CREATE_SCORE_TABLE, CREATE_TEAM_TABLE, TABLE_EXISTS_QUERY } = require(".
 const { Client } = require("pg");
 
 initialize().then(() => {
-    console.log("Schedule load completed!");
+    console.log("Schedule loader completed!");
     process.exit();
 }).catch(
     e => console.error(e.stack)
 );
 
 async function initialize() {
+    console.log("Schedule loader running.");
+    console.log("Initializing tables...");
+
     // initialize client
     const client = new Client();
     await client.connect();
 
-    // load team
+    // creating team
     await createTable(client, 'team', CREATE_TEAM_TABLE);
 
-    // load scores
+    // create scores
     await createTable(client, 'score', CREATE_SCORE_TABLE);
+
+    console.log("Tables initialized.");
 
     // close connection
     await client.end();
