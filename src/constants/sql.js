92c897da802c48113e9ba4b5cb4511012a4596d5
edgrams <edgramswork@gmail.com>
2019-02-11 20:04:12 -0600

    score table

--- src/constants/sql.js
+++ src/constants/sql.js
@@ -1,9 +1,26 @@
 module.exports = {
+    CREATE_GAME_TABLE: "CREATE TABLE game (" +
+        "id SMALLINT PRIMARY KEY, " +
+        "abbreviation CHAR(3) NOT NULL, " +
+        "name VARCHAR(30) NOT NULL " +
+        ");",
+
+    CREATE_SCORE_TABLE: "CREATE TABLE score (" +
+            "id SERIAL PRIMARY KEY, " +
+            "quarter_one SMALLINT NOT NULL, " +
+            "quarter_two SMALLINT NOT NULL, " +
+            "quarter_three SMALLINT NOT NULL, " +
+            "quarter_four SMALLINT NOT NULL, " +
+            "quarter_overtime SMALLINT, " +
+            "total SMALLINT NOT NULL" +
+        ");",
+
     CREATE_TEAM_TABLE: "CREATE TABLE team (" +
             "id SMALLINT PRIMARY KEY, " +
             "abbreviation CHAR(3) NOT NULL, " +
-            "name VARCHAR(30) NOT NULL " +
+            "name VARCHAR(30) NOT NULL" +
         ");",
+
     TABLE_EXISTS_QUERY: "SELECT EXISTS " +
         "(SELECT * FROM information_schema.tables WHERE table_name = $1)"
 };
--- src/schedule-loader.js
+++ src/schedule-loader.js
@@ -1,17 +1,5 @@
-const { CREATE_TEAM_TABLE, TABLE_EXISTS_QUERY } = require("./constants/sql");
+const { CREATE_SCORE_TABLE, CREATE_TEAM_TABLE, TABLE_EXISTS_QUERY } = require("./constants/sql");
 const { Client } = require("pg");
-
-const initialize = async () => {
-    const client = new Client();
-    await client.connect();
-    const res = await client.query(TABLE_EXISTS_QUERY, ['team']);
-    console.log("response: " + JSON.stringify(res.rows[0]));
-    if (!res.rows[0].exists) {
-        const res = await client.query(CREATE_TEAM_TABLE);
-        console.log("response: " + JSON.stringify(res));
-    }
-    await client.end();
-};
 
 initialize().then(() => {
     console.log("Schedule load completed!");
@@ -19,3 +7,27 @@ initialize().then(() => {
 }).catch(
     e => console.error(e.stack)
 );
+
+async function initialize() {
+    // initialize client
+    const client = new Client();
+    await client.connect();
+
+    // load team
+    await createTable(client, 'team', CREATE_TEAM_TABLE);
+
+    // load scores
+    await createTable(client, 'score', CREATE_SCORE_TABLE);
+
+    // close connection
+    await client.end();
+}
+
+async function createTable(client, table, sql) {
+    const res = await client.query(TABLE_EXISTS_QUERY, [table]);
+    console.log(`${table} exists: ` + JSON.stringify(res.rows[0]));
+    if (!res.rows[0].exists) {
+        const res = await client.query(sql);
+        console.log("response: " + JSON.stringify(res));
+    }
+}
