3ad720e30f64343da148fad3001be6410737fd41
edgrams <edgramswork@gmail.com>
2019-02-14 22:20:32 -0600

    bye for all

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,4 +1,4 @@
-const { QUERY_BYE_BY_TEAM } = require("../sql/schedule");
+const { QUERY_BYE_BY_TEAM_BY_YEAR, QUERY_BYE_BY_YEAR } = require("../sql/schedule");
 const Router = require("express-promise-router");
 const { Pool } = require("pg");
 
@@ -6,17 +6,28 @@ const router = new Router();
 const pool = new Pool();
 
 router.get("/byes", (req, res, next) => {
-    let season = req.query.season;
-    let team = req.query.team;
+    const season = req.query.season;
+    const team = req.query.team;
 
-    pool.query(QUERY_BYE_BY_TEAM, [team, season])
-        .then((result) => {
-            console.log(result.rows[0]);
-            res.send(result.rows[0]);
-        })
-        .catch((err) => {
-            return next(err);
-        });
+    if (team) {
+        pool.query(QUERY_BYE_BY_TEAM_BY_YEAR, [team, season])
+            .then((result) => {
+                console.log(result.rows[0]);
+                res.send(result.rows[0]);
+            })
+            .catch((err) => {
+                return next(err);
+            });
+    } else {
+        pool.query(QUERY_BYE_BY_YEAR, [season])
+            .then((result) => {
+                console.log(result.rows);
+                res.send(result.rows);
+            })
+            .catch((err) => {
+                return next(err);
+            });
+    }
 });
 
 module.exports = router;
