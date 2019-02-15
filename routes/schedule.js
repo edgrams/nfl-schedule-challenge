b159e34108951e46f4608d3ebf519f43edcc7f6e
edgrams <edgramswork@gmail.com>
2019-02-14 20:20:35 -0600

    bye endpoint

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,4 +1,4 @@
-const { CREATE_TEMP_TABLE, INSERT_TEMP_TABLE, QUERY_BYE_BY_TEAM } = require("../sql/schedule");
+const { QUERY_BYE_BY_TEAM } = require("../sql/schedule");
 const Router = require("express-promise-router");
 const { Pool } = require("pg");
 
@@ -9,9 +9,7 @@ router.get("/byes", async (req, res) => {
     let season = req.query.season;
     let team = req.query.team;
 
-/*    const { rows } = await pool.query(CREATE_TEMP_TABLE)
-        .then(pool.query(INSERT_TEMP_TABLE))
-        .then(pool.query(QUERY_BYE_BY_TEAM, [team, season]));*/
+    const { rows } = await pool.query(QUERY_BYE_BY_TEAM, [team, season]);
 
     res.send(rows[0]);
 });
--- sql/schedule.js
+++ sql/schedule.js
@@ -1,10 +1,5 @@
 module.exports = {
-    CREATE_TEMP_TABLE: "CREATE TEMP TABLE weeks (week int); ",
-    INSERT_TEMP_TABLE: "INSERT INTO weeks (week) VALUES (1),(2),(3),(4),(5),(6),(7),(8),(9),(10)," +
-        "(11),(12),(13),(14),(15),(16),(17); ",
-    QUERY_BYE_BY_TEAM: "SELECT team.abbreviation, weeks.week FROM team, weeks " +
-            "WHERE team.abbreviation = $1 " +
-            "AND weeks.week NOT IN (SELECT game.week FROM game " +
-                               "WHERE (game.home_team_id = team.id OR game.visitor_team_id = team.id) " +
-                               "AND DATE_PART('year', game.date) = $2);"
+    QUERY_BYE_BY_TEAM: "SELECT team.abbreviation, bye.week FROM team " +
+        "INNER JOIN bye ON team.id = bye.team_id " +
+        "WHERE team.abbreviation = $1 AND bye.season_year = $2;"
 };
