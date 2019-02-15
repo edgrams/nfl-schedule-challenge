1a015bf270464aab8a8127c5d71a7a7c4bbd8223
edgrams <edgramswork@gmail.com>
2019-02-15 00:17:30 -0600

    scores endpoint

--- routes/scores.js
+++ routes/scores.js
@@ -1,4 +1,6 @@
-const { QUERY_SCORE_AVG_BY_TEAM_BY_YEAR } = require("../sql/scores");
+const { QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_ONE, QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_TWO,
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_THREE, QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_FOUR,
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR } = require("../sql/scores");
 const Router = require("express-promise-router");
 const { Pool } = require("pg");
 
@@ -10,6 +12,58 @@ router.get("/scores/:season/team/:team", (req, res, next) => {
     const team = req.params.team;
 
     pool.query(QUERY_SCORE_AVG_BY_TEAM_BY_YEAR, [team, season])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+router.get("/scores/:season/team/:team/quarter/1", (req, res, next) => {
+    const season = req.params.season;
+    const team = req.params.team;
+
+    pool.query(QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_ONE, [team, season])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+router.get("/scores/:season/team/:team/quarter/2", (req, res, next) => {
+    const season = req.params.season;
+    const team = req.params.team;
+
+    pool.query(QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_TWO, [team, season])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+router.get("/scores/:season/team/:team/quarter/3", (req, res, next) => {
+    const season = req.params.season;
+    const team = req.params.team;
+
+    pool.query(QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_THREE, [team, season])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+router.get("/scores/:season/team/:team/quarter/4", (req, res, next) => {
+    const season = req.params.season;
+    const team = req.params.team;
+
+    pool.query(QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_FOUR, [team, season])
         .then((result) => {
             res.send(result.rows);
         })
--- sql/scores.js
+++ sql/scores.js
@@ -1,6 +1,46 @@
 module.exports = {
     QUERY_SCORE_AVG_BY_TEAM_BY_YEAR: "SELECT team.abbreviation, " +
-        "to_char(AVG(score.total), 'FM999999999.00') AS TOTAL_AVERAGE FROM score " +
+        "to_char(AVG(score.total), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
+        "INNER JOIN team ON score.team_id = team.id " +
+        "INNER JOIN game ON score.game_id = game.id " +
+        "INNER JOIN bye ON team.id = bye.team_id " +
+        "WHERE team.abbreviation = $1 " +
+        "AND game.week > bye.week " +
+        "AND bye.season_year = $2 " +
+        "GROUP BY team.abbreviation;",
+
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_ONE: "SELECT team.abbreviation, " +
+        "to_char(AVG(score.quarter_one), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
+        "INNER JOIN team ON score.team_id = team.id " +
+        "INNER JOIN game ON score.game_id = game.id " +
+        "INNER JOIN bye ON team.id = bye.team_id " +
+        "WHERE team.abbreviation = $1 " +
+        "AND game.week > bye.week " +
+        "AND bye.season_year = $2 " +
+        "GROUP BY team.abbreviation;",
+
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_TWO: "SELECT team.abbreviation, " +
+        "to_char(AVG(score.quarter_two), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
+        "INNER JOIN team ON score.team_id = team.id " +
+        "INNER JOIN game ON score.game_id = game.id " +
+        "INNER JOIN bye ON team.id = bye.team_id " +
+        "WHERE team.abbreviation = $1 " +
+        "AND game.week > bye.week " +
+        "AND bye.season_year = $2 " +
+        "GROUP BY team.abbreviation;",
+
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_THREE: "SELECT team.abbreviation, " +
+        "to_char(AVG(score.quarter_three), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
+        "INNER JOIN team ON score.team_id = team.id " +
+        "INNER JOIN game ON score.game_id = game.id " +
+        "INNER JOIN bye ON team.id = bye.team_id " +
+        "WHERE team.abbreviation = $1 " +
+        "AND game.week > bye.week " +
+        "AND bye.season_year = $2 " +
+        "GROUP BY team.abbreviation;",
+
+    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_FOUR: "SELECT team.abbreviation, " +
+        "to_char(AVG(score.quarter_four), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
         "INNER JOIN bye ON team.id = bye.team_id " +
