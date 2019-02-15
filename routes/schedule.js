7937b6f108fb0bac2e614c0065f19a1a617b48ed
edgrams <edgramswork@gmail.com>
2019-02-14 22:33:16 -0600

    route by param

--- routes/schedule.js
+++ routes/schedule.js
@@ -5,27 +5,29 @@ const { Pool } = require("pg");
 const router = new Router();
 const pool = new Pool();
 
-router.get("/byes", (req, res, next) => {
-    const season = req.query.season;
-    const team = req.query.team;
+router.get("/byes/:season", (req, res, next) => {
+    const season = req.params.season;
 
-    if (team) {
-        pool.query(QUERY_BYE_BY_TEAM_BY_YEAR, [team, season])
-            .then((result) => {
-                res.send(result.rows[0]);
-            })
-            .catch((err) => {
-                return next(err);
-            });
-    } else {
-        pool.query(QUERY_BYE_BY_YEAR, [season])
-            .then((result) => {
-                res.send(result.rows);
-            })
-            .catch((err) => {
-                return next(err);
-            });
-    }
+    pool.query(QUERY_BYE_BY_YEAR, [season])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            return next(err);
+        });
+});
+
+router.get("/byes/:season/team/:team", (req, res, next) => {
+    const season = req.params.season;
+    const team = req.params.team;
+
+    pool.query(QUERY_BYE_BY_TEAM_BY_YEAR, [team, season])
+        .then((result) => {
+            res.send(result.rows[0]);
+        })
+        .catch((err) => {
+            return next(err);
+        });
 });
 
 module.exports = router;
