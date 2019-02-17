e9f35642a08b65eaafb387d833a40cbe858a7c5a
edgrams <edgramswork@gmail.com>
2019-02-17 12:16:40 -0600

    load all seasons

---
+++ load-all-seasons.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+./schedule-loader.sh $1 $2 $3 $4 $5 POST 2018
+./schedule-loader.sh $1 $2 $3 $4 $5 POST 2017
+./schedule-loader.sh $1 $2 $3 $4 $5 POST 2016
+./schedule-loader.sh $1 $2 $3 $4 $5 POST 2015
+./schedule-loader.sh $1 $2 $3 $4 $5 POST 2014
+
+./schedule-loader.sh $1 $2 $3 $4 $5 PRE 2018
+./schedule-loader.sh $1 $2 $3 $4 $5 PRE 2017
+./schedule-loader.sh $1 $2 $3 $4 $5 PRE 2016
+./schedule-loader.sh $1 $2 $3 $4 $5 PRE 2015
+./schedule-loader.sh $1 $2 $3 $4 $5 PRE 2014
+
+./schedule-loader.sh $1 $2 $3 $4 $5 PRO 2018
+./schedule-loader.sh $1 $2 $3 $4 $5 PRO 2017
+./schedule-loader.sh $1 $2 $3 $4 $5 PRO 2016
+./schedule-loader.sh $1 $2 $3 $4 $5 PRO 2015
+./schedule-loader.sh $1 $2 $3 $4 $5 PRO 2014
+
+./schedule-loader.sh $1 $2 $3 $4 $5 REG 2018
+./schedule-loader.sh $1 $2 $3 $4 $5 REG 2017
+./schedule-loader.sh $1 $2 $3 $4 $5 REG 2016
+./schedule-loader.sh $1 $2 $3 $4 $5 REG 2015
+./schedule-loader.sh $1 $2 $3 $4 $5 REG 2014
