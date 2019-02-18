83616626f3b4be6c54c7b1730e27c35e526fa406
Ed Grams <edgramswork@gmail.com>
2019-02-18 09:58:51 -0600

    cleanup sql (#19)

--- src/sql/scores.js
+++ src/sql/scores.js
@@ -1,5 +1,5 @@
 module.exports = {
-    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR: "SELECT team.abbreviation, " +
+    QUERY_SCORE_AVG_BY_YEAR_BY_TEAM: "SELECT team.abbreviation, " +
         "to_char(AVG(score.total), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
@@ -10,7 +10,7 @@ module.exports = {
         "AND bye.season_year = $2 " +
         "GROUP BY team.abbreviation;",
 
-    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_ONE: "SELECT team.abbreviation, " +
+    QUERY_SCORE_AVG_BY_YEAR_BY_TEAM_BY_QUARTER_ONE: "SELECT team.abbreviation, " +
         "to_char(AVG(score.quarter_one), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
@@ -21,7 +21,7 @@ module.exports = {
         "AND bye.season_year = $2 " +
         "GROUP BY team.abbreviation;",
 
-    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_TWO: "SELECT team.abbreviation, " +
+    QUERY_SCORE_AVG_BY_YEAR_BY_TEAM_BY_QUARTER_TWO: "SELECT team.abbreviation, " +
         "to_char(AVG(score.quarter_two), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
@@ -32,7 +32,7 @@ module.exports = {
         "AND bye.season_year = $2 " +
         "GROUP BY team.abbreviation;",
 
-    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_THREE: "SELECT team.abbreviation, " +
+    QUERY_SCORE_AVG_BY_YEAR_BY_TEAM_BY_QUARTER_THREE: "SELECT team.abbreviation, " +
         "to_char(AVG(score.quarter_three), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
@@ -43,7 +43,7 @@ module.exports = {
         "AND bye.season_year = $2 " +
         "GROUP BY team.abbreviation;",
 
-    QUERY_SCORE_AVG_BY_TEAM_BY_YEAR_BY_QUARTER_FOUR: "SELECT team.abbreviation, " +
+    QUERY_SCORE_AVG_BY_YEAR_BY_TEAM_BY_QUARTER_FOUR: "SELECT team.abbreviation, " +
         "to_char(AVG(score.quarter_four), 'FM999999999.00') AS POINT_AVERAGE FROM score " +
         "INNER JOIN team ON score.team_id = team.id " +
         "INNER JOIN game ON score.game_id = game.id " +
